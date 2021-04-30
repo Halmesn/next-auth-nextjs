daf0bd5d9821fc1100629e335c0d9abedf33e085
Adrian Li <xiaxi.li.syd@gmail.com>
2021-04-30 22:14:35 +1000

    use Providers warpper by next-auth to reduce http requests

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
+import Providers from 'next-auth/providers';
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Providers session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Providers>
   );
 }
 
